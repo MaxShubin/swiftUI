f69b80caa33326c56192ec0d3cf2eb4fb432badc
MaxShubin <41105902+MaxShubin@users.noreply.github.com>
2022-02-20 17:15:50 +0300

    Add files via upload

---
+++ FirstProject/FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Max on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
