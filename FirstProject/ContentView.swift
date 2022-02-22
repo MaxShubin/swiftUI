f94d4cdba3057d321e3592c94532b8ed3cee542d
Max Shubin <ghthgf@mail.ru>
2022-02-22 14:49:39 +0300

    Initial Commit

---
+++ FirstProject/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  FirstProject
+//
+//  Created by Max on 19.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Max Shubin")
+            .font(.largeTitle)
+            .padding()
+            .background(Color.yellow)
+            .padding()
+            .background(Color.purple)
+            .padding()
+            .background(Color.white)
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
